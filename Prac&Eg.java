9897f60f48562925334c664797b2ee526702cee0
Avaneesh Kumar Patel <AvaneeshKumarPatel>
2025-06-03 16:28:02 +0530

    added if else if and nested if

--- Prac&Eg.java
+++ Prac&Eg.java
@@ -8,3 +8,37 @@ public class demo1
 
     }
 }
+
+
+// Nested else if 
+
+
+package practicejava;
+
+public class PracticeJava {
+
+	public static void main(String[] args)  {
+
+	
+	 int num1 = 40;
+	 int num2 = 20;
+	 int num3 = 30;
+	 
+	  if(num1  > num2 ) {
+		  	
+		  System.out.println("Number1 is greater than number 2");
+		  
+		  
+		if(num2<num3) {
+			
+			System.out.println("Inside Nested if, Jo bhe result hai print kar do  ");
+		}
+		else {
+			System.out.println("Nested else inside block");
+		}
+		
+	  }
+	  
+		
+	}
+}	
