2e17f4d025dee27564825c1bfcb24fc17b4f9876
Avaneesh Kumar Patel <AvaneeshKumarPatel>
2025-06-04 16:35:41 +0530

    Java loops with example

--- Prac&Eg.java
+++ Prac&Eg.java
@@ -41,4 +41,138 @@ public class PracticeJava {
 	  
 		
 	}
-}	
+}
+
+
+
+
+int day = 4;
+switch (day) {
+  case 1:
+    System.out.println("Monday");
+    break;
+  case 2:
+    System.out.println("Tuesday");
+    break;
+  case 3:
+    System.out.println("Wednesday");
+    break;
+  case 4:
+    System.out.println("Thursday");
+    break;
+  case 5:
+    System.out.println("Friday");
+    break;
+  case 6:
+    System.out.println("Saturday");
+    break;
+  case 7:
+    System.out.println("Sunday");
+    break;
+}
+
+
+
+// while loop
+
+package practicejava;
+
+public class WhileLoopJava {
+
+	public static void main(String[] args) {
+		
+		
+		int i = 1;
+		
+		while(i<=0) {
+			
+			System.out.println(i + "Hello");
+			 i++;
+		}
+		
+		
+		//  Even number appraoch 1
+		
+		
+		int i = 2;
+		
+	   while(i<=10) {
+		   
+		   System.out.println(i);
+		   i+=2;
+		   
+	   }
+	   
+		
+		
+		
+	   // Approcah 2
+		
+		
+		
+		int i = 1;
+
+		while (i <= 10) {
+
+			if (i % 2 == 0) {
+				System.out.println(i);
+			}
+
+			i++;
+		}
+
+		
+		// Example 4 : 1.............10
+		
+//		1 odd
+//		2 even
+//		3 odd
+//		4 even
+//		..10even
+		
+		
+	   int i = 1;
+		
+	  while(i<=10) {
+		  
+		  if(i%2==0) 
+		  {
+			  System.out.println(i+ "Even");
+		  }
+		  else
+		  {
+			  System.out.println(i+"Odd");
+		  }
+		  i++;
+		  
+	  }
+		
+	
+		
+	}
+
+}
+
+
+
+// Do while loop
+
+ublic static void main(String[] args) {
+		
+		
+		//example 1 : 1..........10 number print 
+		
+		int i  = 1;
+		
+		do
+		{
+			System.out.println(i);
+			i++;
+		}while(i<=10);
+		
+	}
+
+
+
+
+
