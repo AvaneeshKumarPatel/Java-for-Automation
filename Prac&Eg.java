9f9085a4c093dc2f76690547ed7b6ff7b2bc27b4
Avaneesh Kumar Patel <AvaneeshKumarPatel>
2025-06-05 19:56:16 +0530

    Java array added

--- Prac&Eg.java
+++ Prac&Eg.java
@@ -175,4 +175,111 @@ ublic static void main(String[] args) {
 
 
 
+// Arrays and opration on array 
+
+
+public class ArraysJava {
+
+	public static void main(String[] args) {
+
+		
+		//  Declaring array 
+		
+		// approach 1
+		
+		int a[]= new int[5];
+		
+		a[0]=10;
+		a[2]=30;
+		a[3]=60;
+		a[4]=40;
+		
+		System.out.println(a[3]);
+		
+		// Approach 2  most preferded 
+		
+	int a[] = {1,2,3,4,5,6,7,8,9};
+	
+	// find length of an array 
+	  System.out.println( "Lendth of an array:"+ a.length);
+	  // read single value from an array 
+	 // System.out.println( a[1]);
+		
+	  // reading all the value of an array 
+	  
+	  Normanl for loop 
+	  for( int i=0; i<a.length-1; i++)   // i<=4 i<5 i<=a.length-1  i<a.length
+	  {
+	
+		System.out.println(a[i]);
+//	  }
+	  
+	  // enhanced for loop . for... each loop 
+	  
+	  for(int x:a)
+	  {
+		  System.out.println(x);
+	  }
+	}
+
+}
+
+
+
+// Examples with createing and  opration in multi dimensional array:-
+
+public class TwoDimentionalArray {
+
+	public static void main(String[] args) {
+
+		//  Declearing array 
+		
+		// aproach1
+		
+    	int a[][]=new int[3][2];
+    	int []a[]=new int[3][2];   you can make like this also 
+    	int [][]a=new int[3][2];     you can make like this also 
+		
+		
+ 		a[0][0] =100;
+ 		a[0][1] =200;
+ 		
+ 		a[1][0] = 300;
+ 		a[1][1] = 400;
+ 		
+ 		a[2][0] = 500;
+ 		a[2][1] = 600;
+		
+		
+		// Approach2 
+		
+		int a[][] = { {100,200},
+				      {300,400},
+				      {500,600},
+				      {700,800} 
+		                        };
+		                        
+		// find size of an array
+		
+      		System.out.println("lendth of rows:"+a.length);
+      		System.out.println("lendth of columns:"+a[0].length);
+		
+		// read single value from an array 
+      		
+          System.out.println(a[2][1]);   // 600
+      
+      
+          // normal for getting all element of array , row and column 
+          for(int r=0; r<=2;r++)               // 0
+          {
+        	  for(int c =0; c<=1; c++)         // 0 1 2
+        	  {
+        		  System.out.print(a[r][c]+ " ");   // 100 200
+        	  }
+        	  System.out.println();
+          }
+          
+		
+	}
+
 
